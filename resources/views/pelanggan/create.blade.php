9f36d57204571b7b7f763e5ecb04aa23f174657e
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2024-04-24 17:07:36 +0700

    first commit

---
+++ resources/views/pelanggan/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.main', ['title' => 'Pelanggan'])
+
+@section('title-content')
+    <i class="fas fa-users mr-2"></i>
+    Pelanggan
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-xl-4 col-lg-6">
+            <form method="POST" action="{{ route('pelanggan.store') }}" class="card card-orange card-outline">
+                @csrf
+
+                <div class="card-header">
+                    <h3 class="card-title">Buat Pelanggan Baru</h3>
+                </div>
+
+                <div class="card-body">
+                    <!-- Tambahkan logika untuk menampilkan pesan error -->
+                    @if (session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+
+                    <div class="form-group">
+                        <label>Nama Lengkap</label>
+                        <x-input name="nama" type="text" />
+                    </div>
+
+                    <div class="form-group">
+                        <label>Alamat</label>
+                        <x-textarea name="alamat" />
+                    </div>
+
+                    <div class="form-group">
+                        <label>Nomor Telepon/HP</label>
+                        <x-input name="nomor_tlp" type="text" />
+                    </div>
+                </div>
+
+                <div class="card-footer form-inline">
+                    <button type="submit" class="btn btn-outline-primary">Simpan Pelanggan</button>
+                    <a href="{{ route('pelanggan.index') }}" class="btn btn-secondary ml-auto">Batal</a>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
